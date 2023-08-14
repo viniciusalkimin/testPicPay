0ca4058a18d56044b4a07cb7bb6f1652d485cacf
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-08-14 18:36:58 -0300

    Criação do projeto base com Spring Initializr

---
+++ src/main/java/br/com/picpay/pagamentosimplificado/PagamentoSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay.pagamentosimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoSimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoSimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/picpay/pagamentosimplificado/PagamentoSimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay.pagamentosimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoSimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
