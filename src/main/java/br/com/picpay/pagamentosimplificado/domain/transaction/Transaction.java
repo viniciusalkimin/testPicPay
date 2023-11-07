ad275d367fc47589d16f134d4f67e726b3f0b44b
viniciusalkimin <vgoncalves.deal@alelo.com.br>
2023-11-07 17:16:13 -0300

    Add entity transaction to create table on embedded h2 server

--- src/main/java/br/com/picpay/pagamentosimplificado/domain/transaction/Transaction.java
+++ src/main/java/br/com/picpay/pagamentosimplificado/domain/transaction/Transaction.java
@@ -1,25 +1,32 @@
 package br.com.picpay.pagamentosimplificado.domain.transaction;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Entity
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "transactions")
 public class Transaction {
 
-    private UUID transactionId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
 
     private String payerAccountId;
 
     private String receiverAccountId;
 
-    private BigDecimal value;
+    private BigDecimal transactionValue;
 
-    private LocalDateTime timestamp;
-
+    private LocalDateTime moment;
 }
