4981010bbea16d5f45681edc78498d59699a4cd2
viniciusalkimin <vgoncalves.deal@alelo.com.br>
2023-11-09 12:00:58 -0300

    Add tests of UserController

--- src/test/java/br/com/picpay/pagamentosimplificado/infrastructure/user/UserControllerTest.java
+++ src/test/java/br/com/picpay/pagamentosimplificado/infrastructure/user/UserControllerTest.java
@@ -1,7 +1,58 @@
 package br.com.picpay.pagamentosimplificado.infrastructure.user;
 
-import static org.junit.jupiter.api.Assertions.*;
+import br.com.picpay.pagamentosimplificado.domain.user.dto.UserCreatedRecord;
+import br.com.picpay.pagamentosimplificado.infrastructure.user.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    public UserServiceImpl userService;
+
+    @MockBean
+    public UserCreatedRecord userCreatedRecord;
+    public static final String path = "/pagamento-simplificado/api/users";
+
+    @Test
+    void deveRetornar200CriarUsuario() throws Exception{
+        String requestBody = "{\"completeName\":\"ViniAlkimin\",\"userType\":\"INDIVIDUAL_USER\",\"document\":\"12345684\",\"email\":\"v@email.com\",\"password\":\"12345\"}";
+        when(userService.createUser(any())).thenReturn(userCreatedRecord);
+        mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)).andExpect(status().isOk());
+    }
+
+    @Test
+    void deveRetornar400CriarUsuario() throws Exception{
+        String requestBody = """
+                {
+                    "completeName": "",
+                    "userType": "INDIVIDUAL_USER",
+                    "document": "12345684",
+                    "email" : "v@email.com",
+                    "password": "12345"
+                }
+                """;
+        when(userService.createUser(any())).thenReturn(userCreatedRecord);
+        mockMvc.perform(post(path).contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)).andExpect(status().isBadRequest());
+    }
 
 }
